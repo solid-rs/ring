af1fcc56275eca3a0604102e225ed7b263bdebe8
Tomoaki Kawada <kawada@kmckk.co.jp>
2025-03-24 15:57:45 +0900

    Add pre-generated files

--- build.rs
+++ build.rs
@@ -310,7 +310,7 @@ fn ring_build_rs_main(c_root_dir: &Path, core_name_and_version: &str) {
     let endian = env::var("CARGO_CFG_TARGET_ENDIAN").unwrap();
     let is_little_endian = endian == "little";
 
-    let is_git = fs::metadata(c_root_dir.join(".git")).is_ok();
+    let is_git = false; // fs::metadata(c_root_dir.join(".git")).is_ok();
 
     // Published builds are always built in release mode.
     let is_debug = is_git && env::var("DEBUG").unwrap() != "false";
